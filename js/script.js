bcc6fddf5ca8c05ddcc6f6366e2f0676e1f55491
Piotr Michalczuk <piotrekmichal91@gmail.com>
2016-12-06 11:46:01 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(function() {
+
+	var $nav = $('nav');
+	var $link = $('li a');
+	var $more = $('#banner a');
+	var $menuIcon = $('.chamburger');
+	var $window = $(window);
+	var navHeight = $nav.outerHeight();
+
+	function smoothScroll(e) {
+		e.preventDefault();
+		var elem = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(elem).offset().top
+		}, 500, 'swing');
+	}
+
+	$more.add($link).on('click', smoothScroll);
+
+	$window.on('scroll', function() {
+		var $scrollPos = $window.scrollTop();
+
+		var $section = $('section');
+		$('header').add($section).each(function() {
+			var $this = $(this);
+			if($this.offset().top < ($scrollPos + navHeight) && 
+				($scrollPos + navHeight) < ($this.offset().top + $this.outerHeight())) {
+				var targetClass = '.' + $this.attr('id');
+				$link.removeClass('active');
+				$(targetClass).addClass('active');
+			};
+		});
+	});
+
+	$menuIcon.on('click', function() {
+		$(this).next().toggleClass('open');
+	})
+});
